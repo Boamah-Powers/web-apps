add0ef469013a2e65eeb0eee7c83c1c18cc81394
Boamah-Powers <kb4242@nyu.edu>
2024-07-01 10:08:40 +0000

    adding mongodb and mongoose to node.js

--- bookstore-MERN-stack/backend/config.js
+++ bookstore-MERN-stack/backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const  mongoDBURL = 
+    'mongodb+srv://kb4242:9bUOcVQOkd6u3zG9@bookstore-mern.w5l6edk.mongodb.net/books-collection retryWrites=true&w=majority&appName=bookstore-MERN'
