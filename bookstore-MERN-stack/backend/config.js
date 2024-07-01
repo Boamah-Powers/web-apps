ffccd412afe9f734188d938c70ab7b3453f6ef89
Boamah-Powers <kb4242@nyu.edu>
2024-07-01 15:14:59 +0000

    security fixes

--- bookstore-MERN-stack/backend/config.js
+++ bookstore-MERN-stack/backend/config.js
@@ -1,4 +1,7 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url)
+require('dotenv').config();
+
 export const PORT = 5555;
 
-export const  mongoDBURL = 
-    'mongodb+srv://kb4242:9bUOcVQOkd6u3zG9@bookstore-mern.w5l6edk.mongodb.net/books-collection retryWrites=true&w=majority&appName=bookstore-MERN'
+export const  mongoDBURL = process.env.mongoDBURL;
