372b140bbc59c7849be06df65318d84a0ed6b4ea
Kwaaku Boamah-Powers <120575906+Boamah-Powers@users.noreply.github.com>
2024-07-01 10:16:13 +0000

    Delete bookstore-MERN-stack/backend/config.js

--- bookstore-MERN-stack/backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const  mongoDBURL = 
-    'mongodb+srv://kb4242:9bUOcVQOkd6u3zG9@bookstore-mern.w5l6edk.mongodb.net/books-collection retryWrites=true&w=majority&appName=bookstore-MERN'
