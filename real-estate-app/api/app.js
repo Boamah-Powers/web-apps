d7c1e7cb408dd870ecede5604e4dbacaeea51f14
Boamah-Powers <kb4242@nyu.edu>
2024-07-09 14:00:00 +0000

    initial commit

---
+++ real-estate-app/api/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import postRouter from "./routes/post.route.js"
+
+const app = express();
+
+app.use("/api/posts", postRouter);
+
+app.listen(8800, () => {
+    console.log("Server is running!");
+});
---
+++ real-estate-app/api/routes/post.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.get("/test", (request, response) => {
+//     console.log("router works!");
+// });
+
+// router.post("/test", (request, response) => {
+//     console.log("router works!");
+// });
+
+// router.put("/test", (request, response) => {
+//     console.log("router works!");
+// });
+
+// router.delete("/test", (request, response) => {
+//     console.log("router works!");
+// });
+
+export default router;
