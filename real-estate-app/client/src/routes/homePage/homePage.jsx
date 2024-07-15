ea93b075c874d27cba3975bf14b3ac967b703799
Boamah-Powers <kb4242@nyu.edu>
2024-07-15 13:00:00 +0000

    react responsive hero section design

---
+++ real-estate-app/client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,41 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque ex lectus, ultrices et ante vel, placerat scelerisque
+            ligula. Aliquam rhoncus pretium porttitor. Duis in rhoncus tellus,
+            sit amet tincidunt risus. Ut id diam lobortis, sagittis risus non,
+            pellentesque tellus.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Awards Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
