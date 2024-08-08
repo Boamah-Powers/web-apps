739406ea4b8981dc832afb72b859e8681492b77c
Boamah-Powers <kb4242@nyu.edu>
2024-08-08 16:07:46 +0000

    fetching data based on url query

--- real-estate-app/api/controllers/post.controller.js
+++ real-estate-app/api/controllers/post.controller.js
@@ -2,7 +2,19 @@ import prisma from "../lib/prisma.js"
 
 export const getPosts = async (request, response) => {
     try {
-        const posts = await prisma.post.findMany();
+        const query = request.query;
+        const posts = await prisma.post.findMany({
+            where: {
+                city: query.city || undefined,
+                type: query.type || undefined,
+                property: query.property || undefined,
+                bedroom: parseInt(query.bedroom) || undefined,
+                price: {
+                    gte: parseInt(query.minPrice) || 0,
+                    lte: parseInt(query.maxPrice) || 10000000,
+                }
+            }
+        });
         
         response.status(200).json(posts);
     } catch (error) {
--- real-estate-app/client/src/lib/loaders.js
+++ real-estate-app/client/src/lib/loaders.js
@@ -10,3 +10,24 @@ export const singlePageLoader = ({ request, params }) => {
       console.log(error);
     });
 };
+
+export const listPageLoader = ({ request, params }) => {
+  const query = request.url.split("?")[1]
+  return apiRequest
+    .get("/posts?" + query)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  
+  // return apiRequest
+  //   .get("/posts/" + params.id)
+  //   .then((response) => {
+  //     return response.data;
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+};
