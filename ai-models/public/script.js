abef4770bbca09b467c2a878ad878d10cfc4de30
Boamah-Powers <kb4242@nyu.edu>
2024-10-14 16:26:34 -0400

    completed text-to-image interface

--- ai-models/public/script.js
+++ ai-models/public/script.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 	// Check if a div with the class 'summarizer' exists
 	const summarizerDiv = document.querySelector(".summarizer");
+    const textToImageDiv = document.querySelector(".text-to-image");
 
 	if (summarizerDiv) {
 		// Get references to the textarea and submit button
@@ -24,4 +25,26 @@ document.addEventListener("DOMContentLoaded", () => {
 		// Add an input event listener to validate dynamically on user input
 		textarea.addEventListener("input", validateInput);
 	}
+
+    if (textToImageDiv) {
+        const submitButton = textToImageDiv.querySelector(
+          '.left-editor form input[type="submit"]'
+        );
+        const textarea = textToImageDiv.querySelector(
+			".left-editor form textarea"
+		);
+
+        // Function to validate input and enable/disable submit button
+		const validateInput = () => {
+			const textLength = textarea.value.trim().length;
+			submitButton.disabled = !(textLength > 15 && textLength < 100000);
+		};
+
+		// Run initial validation in case textarea already has text
+		validateInput();
+
+		// Add an input event listener to validate dynamically on user input
+		textarea.addEventListener("input", validateInput);
+    
+      }
 });
--- ai-models/routes/text-to-image.route.js
+++ ai-models/routes/text-to-image.route.js
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { response } from "express";
+import { generateImage } from "../api/text-to-image.api.js";
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('text-to-image.hbs');
-})
+});
+
+router.post('/', (req, res) => {
+    const text = req.body.formText;
+
+    generateImage(text)
+        .then(response => {
+            return Buffer.from(response, 'binary').toString('base64');
+        })
+        .then(base64Image => {
+            const imageUrl = `data:image/jpeg;base64,${base64Image}`;
+            res.render('text-to-image', {originalText: text, imgURL: imageUrl});
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
 
 export default router;
